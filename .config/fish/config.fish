8d6b208c336a71da21b4e8836ae3ddae75422c19
Ravi Banagere <79131947+banagere@users.noreply.github.com>
2025-08-10 23:25:43 +0530

    fish: bug fixes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,27 +1,34 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+    # intial
+    set fish_greeting
+    eval (/opt/homebrew/bin/brew shellenv)
 
-# Aliases
-alias bbc="brew cleanup && brew autoremove && brew update && brew upgrade"
-alias l="ls -lh"
-alias ll="ls -alh"
+    # Aliases
+    alias bbc="brew cleanup && brew autoremove && brew update && brew upgrade"
+    alias l="ls -lh"
+    alias ll="ls -alh"
 
-# Load tide if installed
-#if functions -q tide
-#    tide
-#end
-
-# zoxide init
-if type -q zoxide
-    zoxide init fish | source
-end
-
-# Interactive jump using fzf
-function zi
-    zoxide query --interactive | read -l dir
-    if test -n "$dir"
-        cd "$dir"
+    # zoxide init
+    if type -q zoxide
+        zoxide init fish | source
     end
+
+    # Interactive jump using fzf
+    function zi
+        zoxide query --interactive | read -l dir
+        if test -n "$dir"
+            cd "$dir"
+        end
+    end
+
+    # Amazon Q CLI (add path where `q` binary lives)
+    # Example for brew install:
+    set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+    # If installed elsewhere, update with correct dir:
+    # set -U fish_user_paths /path/to/q/bin $fish_user_paths
+
+    # Load tide if installed
+    #if functions -q tide
+    #    tide
+    #end
 end
-set fish_greeting
